34765a9c84b7cdafaf0e9989651aaecb649471d6
vladKB1 <kazlou.uladzislau@gmail.com>
2021-08-23 18:23:30 +0300

    feat: task is done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
+exports.min = function min(array) {
+    if (arguments.length === 0) {
+        return 0;
+    }
+    if (array.length === 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
+    let Min = Infinity;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < Min) {
+            Min = array[i];
+        }
+    }
+    return Min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (arguments.length === 0) {
+        return 0;
+    }
+    if (array.length === 0) {
+        return 0;
+    }
+    let Max = -Infinity;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > Max) {
+            Max = array[i];
+        }
+    }
+    return Max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (arguments.length === 0) {
+        return 0;
+    }
+    if (array.length === 0) {
+        return 0;
+    }
+    let Avg = 0;
+    for (let i = 0; i < array.length; i++) {
+        Avg += array[i];                    
+    }
+    return Avg / array.length;
 }
